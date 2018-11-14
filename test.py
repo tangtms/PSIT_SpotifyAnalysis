cafb12fcfa4b42b34d2181356ae248a1bc0a7995
Nuntavat suthangkornkul <42973803+Nun0289@users.noreply.github.com>
2018-11-14 13:22:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 """test"""
-def main(txt):
-    print(txt)
-    testtest = "WTF"
-    print(testtest+txt)
-main(input())
+def main():
+    testtest = "WTF P SIT"
+    while 1:
+        print(testtest)
+main()
