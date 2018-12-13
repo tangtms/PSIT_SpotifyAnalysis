f3fb67d595ba289440e977f7a33a33c2cd79e447
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2018-12-13 15:35:02 +0700

    year csv to dict

---
+++ convert/data_dict_year.py
@@ -0,0 +1,18 @@
+"""
+PSIT - music analysis
+turn year csv into dict
+"""
+import csv
+
+data_dict = {}
+def check():
+    with open('./convert/year/regional-th-year-2017-2018.csv', encoding="utf-8") as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        num = 0
+        for i in readCSV:
+            if i[1].lower not in data_dict:
+                data_dict[i[1]] = [int(i[3]), i[2]]
+            else:
+                data_dict[i[1]] = [data_dict[i[1]][3]+int(i[3]), data_dict[i[1]][2]]
+    print(data_dict)
+check()
