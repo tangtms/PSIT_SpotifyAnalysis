b7dcea5896ff7f78dd9b1c362d8effd97b100bbe
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2018-12-13 18:15:55 +0700

    csv month to dict

--- convert/data_dict_month.py
+++ convert/data_dict_month.py
@@ -3,8 +3,9 @@ PSIT - music analysis
 turn month csv into dict
 """
 import csv
+import codecs
 
-data_dict_month = {}
+data_dict = {}
 def check():
     with open('./convert/monthy/regional-th-monthly-2017-09.csv', encoding="utf-8") as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
@@ -14,6 +15,6 @@ def check():
                 data_dict[i[1]] = [int(i[3]), i[2]]
             else:
                 data_dict[i[1]] = [data_dict[i[1]][3]+int(i[3]), data_dict[i[1]][2]]
-    f = open('./visualize/dict_file/dict_month_2017_09', "w")
-    f.write(data_dict_month)
+    f = codecs.open('./visualize/dict_file/dict_month_2017_09.py', "w", "utf-8")
+    f.write(str(data_dict))
 check()
--- convert/data_dict_year.py
+++ convert/data_dict_year.py
@@ -4,7 +4,7 @@ turn year csv into dict
 """
 import csv
 
-data_dict_year = {}
+data_dict = {}
 def check():
     with open('./convert/year/regional-th-year-2017-2018.csv', encoding="utf-8") as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
@@ -14,5 +14,5 @@ def check():
                 data_dict[i[1]] = [int(i[3]), i[2]]
             else:
                 data_dict[i[1]] = [data_dict[i[1]][3]+int(i[3]), data_dict[i[1]][2]]
-    print(data_dict_year)
+    print(data_dict)
 check()
