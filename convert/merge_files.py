971086894f32f5762277ecbd1bae29a989c5894a
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2018-12-13 15:25:34 +0700

    converted year csv

--- convert/merge_files.py
+++ convert/merge_files.py
@@ -9,13 +9,12 @@ input_folder = './convert/monthy'
 output_file = './convert/year/regional-th-year-2017-2018.csv'
 
 def readBatch():
-    """ Read all files in '/dataset/data/tag_counts/topic_based' """
+    """ Read all files in '/convert/monthy' """
     for subdir, _, files in os.walk(input_folder):
         for file in files:
             path = os.path.join(subdir, file)
             with open(path, encoding="utf8") as csvfile:
                 readCSV = csv.reader(csvfile, delimiter=',')
-                next(readCSV, None) #Skip header
                 for row in readCSV:
                     write_file(row)
 
--- test.py
+++
@@ -1,6 +0,0 @@
-"""test"""
-def main():
-    testtest = "WTF P SIT"
-    while 1:
-        print(testtest)
-main()
