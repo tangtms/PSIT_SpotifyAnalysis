e4b17f32dc00ff72dedd7a5b3e3b2c5bc9ce1afe
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2018-12-14 20:08:10 +0700

    dict month fix

--- convert/data_dict_month.py
+++ convert/data_dict_month.py
@@ -7,14 +7,13 @@ import codecs
 
 data_dict = {}
 def check():
-    with open('./convert/monthy/regional-th-monthly-2018-09.csv', encoding="utf-8") as csvfile:
+    with open('./convert/monthy/regional-th-monthly-2017-09.csv', encoding="utf-8") as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
-        num = 0
         for i in readCSV:
-            if i[1].lower not in data_dict:
+            if i[1] not in data_dict:
                 data_dict[i[1]] = [int(i[3]), i[2]]
             else:
-                data_dict[i[1]] = [data_dict[i[1]][3]+int(i[3]), data_dict[i[1]][2]]
-    f = codecs.open('./visualize/dict_file/dict_month_13.py', "w", "utf-8")
+                data_dict[i[1]] = [data_dict[i[1]][0]+int(i[3]), i[2]]
+    f = codecs.open('./visualize/dict_file/dict_month_1.py', "w", "utf-8")
     f.write(str(data_dict))
 check()
