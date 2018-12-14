7c281ea0cb20c3183a348e0cc73fbcedcaa00572
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2018-12-14 22:44:44 +0700

    merge dict_month

--- convert/data_dict_month.py
+++ convert/data_dict_month.py
@@ -7,13 +7,13 @@ import codecs
 
 data_dict = {}
 def check():
-    with open('./convert/monthy/regional-th-monthly-2017-09.csv', encoding="utf-8") as csvfile:
+    with open('./convert/monthy/regional-th-monthly-2018-09.csv', encoding="utf-8") as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for i in readCSV:
             if i[1] not in data_dict:
                 data_dict[i[1]] = [int(i[3]), i[2]]
             else:
                 data_dict[i[1]] = [data_dict[i[1]][0]+int(i[3]), i[2]]
-    f = codecs.open('./visualize/dict_file/dict_month_1.py', "w", "utf-8")
-    f.write(str(data_dict))
+    f = codecs.open('./visualize/dict_file/dict_month.py', "a", "utf-8")
+    f.write("\ndict_month_13 = "+ str(data_dict))
 check()
