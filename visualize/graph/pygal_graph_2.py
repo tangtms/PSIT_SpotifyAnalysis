74ba8e95d87be39b0c4212f91d954ac25d7e35cd
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2018-12-14 22:55:00 +0700

    pygal graph 2

--- visualize/graph/pygal_graph_2.py
+++ visualize/graph/pygal_graph_2.py
@@ -1,8 +1,8 @@
 import pygal
 
 line_chart = pygal.Line()
-line_chart.title = 'Stream count monthly'
-line_chart.x_labels = map(str, range(10, 20))
+line_chart.title = 'Stream Count Monthly'
+line_chart.x_labels = ["Sep-2017", "Oct-2017", "Nov-2017", "Dec-2017", "Jan-2018", "Feb-2018", "March-2018", "April-2018", "May-2018", "June-2018", "Jul-2018", "Aug-2018", "Sep-2018"]
 line_chart.add('Stream',  [3688394, 3121660, 3944840, 4530976, 4582436, 5134192, 5708459, 5732742, 6545542, 7545336, 7659390, 8636828, 9434273])
 line_chart.render()
 
