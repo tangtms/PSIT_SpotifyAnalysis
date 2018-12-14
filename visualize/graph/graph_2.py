dde0c42a2104a9426ed0646034c4512e415a671f
Nuntavat suthangkornkul <42973803+Nun0289@users.noreply.github.com>
2018-12-14 15:08:17 +0700

    graph_2

---
+++ visualize/graph/graph_2.py
@@ -0,0 +1,7 @@
+import pygal
+line_chart = pygal.Line()
+line_chart.title = 'Stream count monthly'
+line_chart.x_labels = map(str, range(2002, 2013))
+line_chart.add('Stream',  [3688394, 3121660, 3944840, 4530976, 4582436, 5134192, 5708459, 5732742, 6545542, 7545336, 7659390, 8636828, 9434273])
+line_chart.render()
+line_chart.render_to_file('./visualize/graph_2.svg')
