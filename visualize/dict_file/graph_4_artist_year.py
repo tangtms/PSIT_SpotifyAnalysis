1a80b0492bfc9fbba7ffa840e0c29d63b7d4f1c2
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2018-12-14 23:46:49 +0700

    graph 4 svg

--- visualize/dict_file/graph_4_artist_year.py
+++ visualize/dict_file/graph_4_artist_year.py
@@ -1,5 +1,6 @@
 import dict_year
 def artist_year():
+    all_steam = 247875921
     data = dict_year._year
     artist_year = {}
     for i in data.keys():
@@ -8,18 +9,25 @@ def artist_year():
         else:
             artist_year[data[i][1]] += data[i][0]
     sort_artist_year = [v[0] for v in sorted(artist_year.items(), key=lambda i: (-i[1], i[0]))]
-    print(sort_artist_year[0], artist_year[sort_artist_year[0]])
-    for i in sort_artist_year:
-        print(i, artist_year[i])
+    #1st artist
+    #print(sort_artist_year[0], artist_year[sort_artist_year[0]])
+
+    #for i in sort_artist_year:
+    #        print(i, artist_year[i])
+    count = 0
+    for i in range(10):
+        count += artist_year[sort_artist_year[i]]
+        print(i+1, sort_artist_year[i], artist_year[sort_artist_year[i]], artist_year[sort_artist_year[i]]/all_steam*100)
+    print(all_steam - count)
 artist_year()
 # ALL 247875921
-# The Toys 8534491
-# Atom Chanakan 7194833
-# BTS 7148502
-# BLACKPINK 6810406
-# Ed Sheeran 5794695
-# Room 39 5389441
-# Post Malone 4247444
-# Charlie Puth 4206773
-# Lauv 3703832
-# Lipta 3429268
+# 1 The Toys 8534491 3.4430496377258044
+# 2 Atom Chanakan 7194833 2.902594560606796
+# 3 BTS 7148502 2.883903354210835
+# 4 BLACKPINK 6810406 2.7475060798664668
+# 5 Ed Sheeran 5794695 2.3377401792891375
+# 6 Room 39 5389441 2.1742495109075155
+# 7 Post Malone 4247444 1.7135363462754416
+# 8 Charlie Puth 4206773 1.697128540371616
+# 9 Lauv 3703832 1.4942282352629161
+# 10 Lipta 3429268 1.3834615263012982
--- visualize/graph/pygal_graph_4.py
+++ visualize/graph/pygal_graph_4.py
@@ -1,11 +1,17 @@
 import pygal
 
-pie_chart = pygal.Pie(inner_radius=.75)
-pie_chart.title = 'Top 10 Artists of the year (in %)'
-pie_chart.add('IE', 19.5)
-pie_chart.add('Firefox', 36.6)
-pie_chart.add('Chrome', 36.3)
-pie_chart.add('Safari', 4.5)
-pie_chart.add('Opera', 2.3)
+pie_chart = pygal.Pie()
+pie_chart.title = 'Top 10 Artists of the year (in Steams)'
+pie_chart.add('Other Artists', 191416236)
+pie_chart.add('The Toys', 8534491)
+pie_chart.add('Atom Chanakan', 7194833)
+pie_chart.add('BTS', 7148502)
+pie_chart.add('BLACKPINK', 6810406)
+pie_chart.add('Ed Sheeran', 5794695)
+pie_chart.add('Room 39 5389441', 5389441)
+pie_chart.add('Post Malone', 4247444)
+pie_chart.add('Charlie Puth', 4206773)
+pie_chart.add('Lauv', 3703832)
+pie_chart.add('Lipta', 3429268)
 
-line_chart.render_to_file('./visualize/graph/graph_4.svg')
+pie_chart.render_to_file('./visualize/graph/graph_4.svg')
