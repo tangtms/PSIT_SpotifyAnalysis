5a630c28d13306a8ed16081085554fe9f304dc60
Thanawat <thanawat.kemwat@gmail.com>
2018-12-14 15:14:34 +0700

    artist_month

---
+++ visualize/dict_file/graph_3_artist_of_month.py
@@ -0,0 +1,13 @@
+import dict_month_1
+def artist_of_month():
+    data = dict_month_1.dict_month_1
+    artist_month = {}
+    for i in data.keys():
+        if data[i][1] not in artist_month:
+            artist_month[data[i][1]] = data[i][0]
+        else:
+            artist_month[data[i][1]] += data[i][0]
+    sort_artist_month = [v[0] for v in sorted(artist_month.items(), key=lambda i: (-i[1], i[0]))]
+    print(sort_artist_month[0], artist_month[sort_artist_month[0]])
+
+artist_of_month()
